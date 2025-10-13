162f33fd3547e3a56e9a38af58c80c814307a110
Max Rantil <rantil@pm.me>
2025-10-13 10:43:08 +0200

    test: intentionally break .zshrc linking to verify CI failure detection

--- install.sh
+++ install.sh
@@ -67,7 +67,8 @@ link_file() {
 
 # Link dotfiles
 link_file "$DOTFILES_DIR/.zprofile" "$HOME/.zprofile"
-link_file "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+# TEST: Intentionally break .zshrc linking to verify CI catches it
+# link_file "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 link_file "$DOTFILES_DIR/.aliases" "$HOME/.aliases"
 
 # Link inputrc for readline (bash, python REPL, etc.)
